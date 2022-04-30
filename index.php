cf245a58a1a46b49c1a48f93fd70fd8a16cc4d6c
0368415649 <kamihime1312@gmail.com>
2022-04-30 13:42:20 +0700

    update

--- index.php
+++ index.php
@@ -6,21 +6,49 @@
     }else{
     include "./views/header.php";
     if(isset($_GET['slug'])){
-        include './sql_data/more.php';
+        if($_GET['slug'] == 'view_cart'){
+            include './views/body/cart_view.php';
+        }else{
+            include './sql_data/more.php';
+        }
+       
     }else{
+        if( !isset($_GET['slug']) && !isset($_GET['route']) ){
+             $color_f = $color_t = $color_c  = '';
+             $color_all= 'content-item-color';
+             if(isset($_GET['category'])){
+                    if($_GET['category'] == 'floor'){
+                    $color_f = 'content-item-color';
+                    $color_all='';
+                    }
+                    if($_GET['category'] == 'desk_table'){
+                        $color_t = 'content-item-color';
+                        $color_all='';
+                    }
+                    if($_GET['category'] == 'ceiling'){
+                        $color_c = 'content-item-color';
+                        $color_all='';
+                    }
+               }
+            
+            
+                echo' <div class="content " id="content">
+                <div class="content_hiden">
+                    <div class="grap header-img ">
+                            <img src="./resources/img/anh1.png" alt="" width="100%">
+                    </div>
+                    <div class="garp">
+                        <ul class="content-list">
+                            <li class="content-item '.$color_all.' "><a href="./"  class="category_a" style="color:black;">ALL</a></li>
+                            <li class="content-item '.$color_t.'"><a href="?category=desk_table"  class="category_d" style="color:black;" >DESK & TABLE</a></li>
+                            <li class="content-item '.$color_f.'"><a href="?category=floor"  class="category_f" style="color:black;" >FLOOR</a></li>
+                            <li class="content-item '.$color_c.'"><a href="?category=ceiling"  class="category_c" style="color:black;" >WALL , PENDANT & CEILING</a></li>
+                        </ul>
+                    </div>
+                </div>';
+        }
     echo '
-            <div class="content " id="content">
-            <div class="grap header-img">
-                    <img src="./resources/img/anh1.png" alt="" width="100%">
-            </div>
-            <div class="garp">
-                <ul class="content-list">
-                    <li class="content-item"><a href="./"  class="category_a" style="color:black;">ALL</a></li>
-                    <li class="content-item"><a href="?category=desk_table"  class="category_d" style="color:black;" >DESK & TABLE</a></li>
-                    <li class="content-item"><a href="?category=floor"  class="category_f" style="color:black;" >FLOOR</a></li>
-                    <li class="content-item"><a href="?category=ceiling"  class="category_c" style="color:black;" >WALL , PENDANT & CEILING</a></li>
-                </ul>
-            </div>
+           
 
             <div class="grap content-main"  >  
                 <div class="row" id="root" >';
@@ -31,6 +59,7 @@
                                     if($_GET['route'] == 'contact'){
                                         include './views/body/contact.php';
                                     }
+                                    
                             }else{
                                 include './sql_data/test.php';
                             }
@@ -43,6 +72,8 @@
     include './views/footer.php';
 }
     ?>
+
+
 <script>
         function change_fw(){
         var item = document.querySelectorAll(".content-item");
--- shipping.php
+++ shipping.php
@@ -1,4 +1,6 @@
 <?php
+
+
     if(isset($_POST['last_name']) && $_POST['last_name'] !=""){
        $last_name = $_POST['last_name'];
        $first_name = $_POST['first_name'];
@@ -20,6 +22,7 @@
          ('".$email."','".$_POST[$name]."','".$_POST[$img]."','".$_POST[$quantily]."')";
              mysqli_query($conn,$sql);
         }
+        header('location:/led_light_project2');
     }
 
 ?>
@@ -31,24 +34,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Document</title>
+    <title id='main_title'>Document</title>
 </head>
 <style>
     .box{
         box-sizing: border-box;
     }
     .shipping_adr{
-        padding: 70px 40px 0px 150px;
+        padding: 0px 40px 0px 50px;
         border-right: 1px solid #d9d9d9;
         height: 100%;
     }
     .shipping_cart{
-        padding: 70px 50px 0px 30px;
-
+        padding: 0px 50px 0px 30px;
+        /* margin-top: 70px;     */
     }
     .shipping_adr_tlt{
         font-weight: 400;
         font-size: 30px;
+        margin-top: 70px;
+
     }
     .shipping_adr_tlt2{
         margin: 20px 0px;
@@ -60,16 +65,16 @@
         padding-left: 5px;
         font-size:14px;
         border-color: #d9d9d9;
+        margin: 8px 0px;
 
     }
-    .shipping_adr_ip_w{
-        width: 40%;
-    }
+
     .mgau{
         margin-right:auto;
     }
     .shipping_adr_ip_gr{
         display: flex;
+        justify-content: space-between;
     }
     .shipping_adr_btn{
         padding: 20px 15px;
@@ -105,64 +110,181 @@
        justify-content: space-between;
        padding-top:30px;
        border-top: 1px solid #d9d9d9;
+       align-items: center;
     }
     .flex_sp{
         display: flex;
         justify-content: space-between;
     }
+    .shipping_data{
+        margin-top: 70px;
+    }
+    .shipping_cart_price{
+            color:#d90606;
+    }
+    a{
+        text-decoration: none;
+    }
+    @keyframes lightly{
+    to{
+        opacity: 1;
+    }
+    from{
+        opacity: 0;
+    }
+    }
+    .container{
+        animation: lightly ease-out 3s;
+    }
 </style>
 <body>
     <div class="container">
         <form action="" method="POST" class="shipping_cart">
-        <div class="row">
+                <div class="row">
 
-            <div class="col-lg-7 box shipping_adr">
-                <div class="text-center shipping_adr_tlt">LUX LED Lighting</div>
-                <div class="fs-5 shipping_adr_tlt2">Contact information</div>
-                <div class="">
-                    <input type="text" name="email" class="shipping_adr_ip" placeholder="Email">
+                    <div class="col-lg-7 box shipping_adr">
+                        <div class="text-center shipping_adr_tlt">LUX LED Lighting <a style="color: #e35820;" href="?slug=view_cart">View_cart</a> </div>
+                        <div class="fs-5 shipping_adr_tlt2">Contact information</div>
+                        <div class="">
+                            <input type="text"  rules='email' name="email" class="shipping_adr_ip" placeholder="Email">
+                            <span class="message"></span>   
+                        </div>
+                        <div class="fs-5 shipping_adr_tlt2">Shipping address</div>
+
+                        <div class="shipping_adr_ip_gr mt-2">
+
+                            <div class="" style="padding: 0px 50px 0px 0px; flex: 1;">
+                                <input type="text"  rules='min:5' name="first_name" class="shipping_adr_ip shipping_adr_ip_w mgau"  placeholder="First name">
+                                <span class="message"></span>   
+                            </div>
+
+                            <div class="" style="padding: 0px 0px 0px 2px; flex: 1;">
+                                <input type="text"  rules='min:5' name="last_name" class="shipping_adr_ip shipping_adr_ip_w "  placeholder="Last name"   >
+                                <span class="message"></span>   
+                            </div>
+
+                        </div>
+
+                        <div class="mt-3">
+                            <input type="text"  rules='min:5' name="address" class="shipping_adr_ip"  placeholder="Address"  >
+                            <span class="message"></span>   
+                        </div>
+                        <div class="mt-3">
+                            <input type="text"  rules='phone' name="phone" class="shipping_adr_ip"  placeholder="Phone"  >
+                            <span class="message"></span>   
+                        </div>
+
+                        
+
+                        <div class="mt-4 d-flex">
+                            <div  class="shipping_adr_btn">Continue to Shipping</div>
+                            <a href="./" class="shipping_adr_btn_cart">Come back home</a>
+                        </div>
+                        <div style="height:200px"></div>
+                    </div>
+
+                    <div class="col-lg-5 box shipping_cart">
+                        <div class="shipping_data">
+
+                        </div>
+                    
+
+                    
+                    <div class="shipping_cart_total">
+                        <div class="fs-5">Total</div>
+                        <div class="fs-3 shipping_cart_price"></div>
+                    </div>
+                    </div>
+
                 </div>
-                <div class="fs-5 shipping_adr_tlt2">Shipping address</div>
-                <div class="shipping_adr_ip_gr mt-2">
-                    <input type="text" name="first_name" class="shipping_adr_ip shipping_adr_ip_w mgau"  placeholder="First name">
-                    <input type="text" name="last_name" class="shipping_adr_ip shipping_adr_ip_w "  placeholder="Last name"   >
-                </div>
-                <div class="mt-3">
-                    <input type="text" name="address" class="shipping_adr_ip"  placeholder="Address"  >
-                </div>
-                <div class="mt-3">
-                    <input type="text" name="phone" class="shipping_adr_ip"  placeholder="Phone"  >
-                </div>
-
-                
-
-                <div class="mt-4 d-flex">
-                    <div  class="shipping_adr_btn">Continue to Shipping</div>
-                    <a href="./" class="shipping_adr_btn_cart">Come back home</a>
-                </div>
-            </div>
-
-            <div class="col-lg-5 box shipping_cart">
-                <div class="shipping_data">
-
-                </div>
-              
-
-              
-               <div class="shipping_cart_total">
-                   <div class="fs-5">Total</div>
-                   <div class="fs-3 shipping_cart_price"></div>
-               </div>
-            </div>
-
-        </div>
         </form>
 
     </div>
+
 </body>
 </html>
 <script>
-    function showCart(){
+    document.querySelector("#main_title").innerHTML = "SHIPPING";
+
+    function validate(data){
+    function requireds(rules,value){
+        if(rules === ''){
+            return '';
+        }
+        if(rules === 'email'){
+            if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)){
+                return '';
+            }else{
+                return 'This is not email';  
+            }
+        }
+        if(rules === 'phone'){
+            if(/^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/.test(value)){
+                return '';
+            }else{
+                return 'This is not number phone';  
+            }
+        }
+        if(rules.split(":")){
+            var min = rules.split(":");
+            if(value.length < min[1]){
+                    return 'Enter at least '+min[1]+' characters';
+                }else{
+                    return '' ;
+                }
+        }
+    }
+
+    var validates = {
+        required: function(value,rules){
+            return value ? requireds(rules,value) :"Please enter this field";
+        }
+    }
+
+    var forms = document.querySelector(data);
+
+    inputs= forms.querySelectorAll('[rules]');  
+
+    inputs.forEach(function(input){
+        input.onblur = function(e){
+            var rules = input.getAttribute('rules');
+            message = e.target.parentElement.querySelector('.message');
+            message.innerHTML =validates.required(e.target.value,rules)
+        }
+        input.oninput = function(e){
+            var rules = input.getAttribute('rules');
+            message = e.target.parentElement.querySelector('.message');
+            message.innerHTML = ''; 
+        }
+    })
+
+    var button = document.querySelector('.shipping_adr_btn');
+    button.onclick = function(){
+            inputs.forEach(function(input){
+                var rules = input.getAttribute('rules');
+                message = input.parentElement.querySelector('.message');
+                message.innerHTML =validates.required(input.value,rules)
+            })
+            messages = document.querySelectorAll('.message');
+            var check = true;
+            messages.forEach(function(e){
+                if(e.innerHTML !== ''){
+                    check = false;
+                    return 1;
+                }
+            })
+            if(check){
+                alert('We have received your application, we will contact you soon, thank you for your purchase');
+                localStorage.removeItem('cart');
+                document.querySelector('.shipping_cart').submit();
+            }
+        }
+}
+
+    validate('.shipping_cart');
+    
+
+function showCart(){
     const storage = localStorage.getItem('cart');
     if(storage){
         cart_data= JSON.parse(storage);
@@ -173,13 +295,13 @@
         cart_data.map((item,ix)=>{
         s+=`
               <div class="row shipping_cart_item">
-                   <div class="col-lg-2 box ">
-                       <img src="${item.product.img}"  alt="" width="100%" height="100%">
+                   <div class="col-lg-3 box ">
+                       <img src="${item.product.img}" style="border: 1px solid #e2d7d7;  border-radius:4px"  alt="" width="100%" height="100%">
                    </div>
-                   <div class="col-lg-10 box">
-                       <div class="">${item.product.band} - ${item.product.name}</div>
-                       <div class="flex_sp">
-                            <div class="">${item.quantity}</div>
+                   <div class="col-lg-9 box">
+                       <div class="" style="font-size:17px; font-weight:bold;color: #black;">${item.product.band} - ${item.product.name}</div>
+                       <div class="flex_sp" style="padding:0px 0px 0px 0px; margin-top:10px; font-size:19px" >
+                            <div class="">Quantily: ${item.quantity}</div>
                             <div class="">$${item.product.price}.00</div>
                       </div>
                        
@@ -200,9 +322,5 @@
     }
 }
 showCart();
-var button = document.querySelector('.shipping_adr_btn');
-button.onclick = function(){
-    localStorage.removeItem('cart');
-    document.querySelector('.shipping_cart').submit();
-}
+
 </script>
